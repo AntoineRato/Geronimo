9b594740c945681895135b2df4c50e065a8b7ed1
antoine <antoine.rato@gmail.com>
2018-08-02 23:15:23 +0200

    Material Changement + Pinch scale
    • Added :
    - UI interface
    - Button to show/hide the material menu
    - Materials menu with somes buttons to change the material of the object on scene
    - Pinch gesture to change the scale of the object on scene

---
+++ Assets/scene_main/scripts/sc_material_manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class sc_material_manager : MonoBehaviour
+{
+    static public Material selected_material;
+    public GameObject materials_menu;
+    public GameObject mainObject;
+
+    private Renderer[] childrensMainObject;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void change_selected_material()
+    {
+        selected_material = Resources.Load("materials/" + EventSystem.current.currentSelectedGameObject.name, typeof(Material)) as Material;
+        Debug.Log("selected material : " + selected_material);
+        childrensMainObject = mainObject.GetComponentsInChildren<Renderer>();
+
+        foreach(Renderer child in childrensMainObject)
+        {
+            var mats = new Material[child.materials.Length];
+            for (var j = 0; j < child.materials.Length; j++)
+            {
+                mats[j] = selected_material;
+            }
+            child.materials = mats;
+        }
+    }
+
+    public void display_materials_menu()
+    {
+        if (materials_menu.activeSelf)
+            materials_menu.SetActive(false);
+        else
+            materials_menu.SetActive(true);
+    }
+}
